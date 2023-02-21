80a8c3bb2db2d66b3360bb2bb3fd9491e126d63e
Jefferson <jeffersonsgoncalves@gmail.com>
2023-02-21 18:03:04 -0300

    Criando Class Livro;

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,16 +4,49 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-    /*    System.out.println("Hello World! ");*/
-
+        /*    System.out.println("Hello World! ");*/
         Gato gato = new Gato("Lili", "Frajola", "13 anos");
         System.out.println(gato);
+        Livro livro = new Livro("Começe pelo porque", 300);
+        System.out.println(livro);
     }
 }
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
 
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
 // refactor shift + f6
-// Identiti =  crtl + alt + l
+// Identity =  crtl + alt + l
 // Duplicar linha   =  Crtl + d
 // Excluir linha   =  Crtl + Y
 
