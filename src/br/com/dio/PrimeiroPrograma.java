8a82beb276d61f7b03daa9382520a73fad5da979
Jefferson <jeffersonsgoncalves@gmail.com>
2023-02-21 17:53:46 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    /*    System.out.println("Hello World! ");*/
+
+        Gato gato = new Gato("Lili", "Frajola", "13 anos");
+        System.out.println(gato);
+    }
+}
+
+
+// refactor shift + f6
+// Identiti =  crtl + alt + l
+// Duplicar linha   =  Crtl + d
+// Excluir linha   =  Crtl + Y
+
+// Mover linha cima   =  Crtl + shift + Seta cima
+// Mover linha baixo  =  Crtl + shift + Seta baixo
+
